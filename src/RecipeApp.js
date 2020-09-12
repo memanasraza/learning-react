52d6f8b732da8691e30baa09ee8718362e7cad9b
memanasraza <memanasraza@gmail.com>
2020-09-12 19:24:55 +0500

    Set up a Hello World App

---
+++ src/RecipeApp.js
@@ -0,0 +1,13 @@
+'use-strict';
+import React from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+const RecipeApp = () => {
+  return <h1> Hello World</h1>
+}
+
+
+
+
+export default RecipeApp;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import AuthorQuiz from './AuthorQuiz';
+import RecipeApp from './RecipeApp';
 import * as serviceWorker from './serviceWorker';
-//import Clock from './newClock'
-//const props = {a:2, b:4}
-
-class Clock extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = this.getTime();
-  }
-  getTime(){
-    const currentTime = new Date(),
-          hours = currentTime.getHours(),
-          minutes = currentTime.getMinutes(),
-          seconds = currentTime.getSeconds(),
-          ampm = hours > 12 ? 'pm' : 'am';
-    return {hours, minutes, seconds, ampm};
-  }
-  setTimer(){
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
-  }
-  updateClock(){
-    const currentTime = new Date();
-    this.setState({currentTime: currentTime}, this.setTimer);
-  }
-  
-
-  render(){
-const {hours, minutes, seconds, ampm} = this.state;
 
 
-    return (
-      <div className="clock">
-        {hours === 0 ? 12 : (hours > 12) ? hours - 12 : hours}
-        :
-        {minutes > 9 ? minutes : `0${minutes}`}
-        :
-        {seconds > 9 ? seconds : `0${seconds}`} {ampm}
-      </div>
-    )
 
-  }
-}
 
 ReactDOM.render(
-  <Clock />,
+  <RecipeApp />,
   document.getElementById('root')
 );
 
